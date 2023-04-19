307f7664013ce243f7ed5d62bb4d401d2f9e172c
Chardyyy07 <102837471+Chardyyy07@users.noreply.github.com>
2023-04-19 18:37:40 +0800

    modified:   Payment System/payment.css 	modified:   Payment System/payment.html 	modified:   Payment System/payment.js 	modified:   Payment System/reciept.html

--- Payment System/payment.js
+++ Payment System/payment.js
@@ -7,22 +7,43 @@ function submitForm() {
     document.getElementById("amount").value = denominationValue;
 }
 
+// Set the pop up window appear if the submit button is clicked
 function generateReceipt() {
-    // Get the selected denomination value
-    let denomination = document.querySelector('input[name="denomination"]:checked').value;
+    // Retrieve values from form inputs
+    var denomination = document.querySelector('input[name="denomination"]:checked').value;
+    var paymentMethod = document.querySelector('input[name="payment_method"]:checked').value;
+    var cardNumber = document.getElementById('card_number').value;
+    var email = document.getElementById('email').value;
 
-    // Get the selected payment method
-    let paymentMethod = document.querySelector('input[name="payment_method"]:checked').value;
+    // Populate the elements in the floating pop-up window
+    document.getElementById('denominationSummary').innerHTML = 'Denomination: ₱' + denomination;
+    document.getElementById('paymentMethodSummary').innerHTML = 'Payment Method: ' + paymentMethod;
+    document.getElementById('cardNumberSummary').innerHTML = 'Card Number: ' + cardNumber;
+    document.getElementById('emailSummary').innerHTML = 'Email: ' + email;
 
-    // Get the email address entered by the user
-    let email = document.getElementById('email').value;
-
-    // Create a receipt string with the selected denomination, payment method, and email address
-    let receipt = `Denomination: ₱${denomination}\nPayment Method: ${paymentMethod}\nEmail: ${email}`;
-
-    // Open a new window with a receipt.html file
-    let receiptWindow = window.open('receipt.html', '_blank');
-
-    // Write the receipt string to the new window
-    receiptWindow.document.write(`<pre>${receipt}</pre>`);
+    // Show the floating pop-up window
+    document.getElementById('orderSummary').style.display = 'block';
 }
+
+function cancelOrder() {
+    // Cancel order logic here
+    closePopup();
+}
+
+function confirmOrder() {
+    // Confirm order logic here
+    closePopup();
+}
+
+function closePopup() {
+    // Hide the floating pop-up window
+    document.getElementById('orderSummary').style.display = 'none';
+}
+
+document.getElementById('paymentForm').addEventListener('submit', function(event) {
+    // Prevent form submission
+    event.preventDefault();
+
+    // Call generateReceipt function to display the floating pop-up window
+    generateReceipt();
+});
